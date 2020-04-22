96836381f0b0c428a9a07746c144f8473354ebfd
LionelB <lionel@lumographe.fr>
2020-04-22 10:26:23 +0200

    fix whitespace in text too

--- src/__tests__/parseDom.test.js
+++ src/__tests__/parseDom.test.js
@@ -5,9 +5,18 @@ import path from "path";
 const { parseDom } = require("../index");
 
 const sample = fs.readFileSync(path.join(__dirname, "article.html")).toString();
+const acd = fs
+  .readFileSync(path.join(__dirname, "agents-chimiques-dangereux-acd.html"))
+  .toString();
 
 test("should parse HTML section", () => {
   const dom = new JSDOM(sample);
+  const parsed = parseDom(dom);
+  expect(parsed).toMatchSnapshot();
+});
+
+test("should remove whitespa HTML section", () => {
+  const dom = new JSDOM(acd);
   const parsed = parseDom(dom);
   expect(parsed).toMatchSnapshot();
 });
