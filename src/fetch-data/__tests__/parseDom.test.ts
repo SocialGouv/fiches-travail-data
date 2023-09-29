a55b16747d89ffead97b492433e0951d4bb2c7db
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-09-29 11:46:22 +0200

    fix: intro

--- src/fetch-data/__tests__/parseDom.test.ts
+++ src/fetch-data/__tests__/parseDom.test.ts
@@ -1,8 +1,8 @@
 import fs from "fs";
-import {JSDOM} from "jsdom";
+import { JSDOM } from "jsdom";
 import path from "path";
 
-import {parseDom} from "../parseDom";
+import { parseDom } from "../parseDom";
 
 const sample = fs.readFileSync(path.join(__dirname, "article.html")).toString();
 
@@ -36,6 +36,10 @@ const pictureHtmlSimple = fs
 
 const articleError = fs
   .readFileSync(path.join(__dirname, "article-error.html"))
+  .toString();
+
+const articleWithScriptIntro = fs
+  .readFileSync(path.join(__dirname, "article-intro-with-script.html"))
   .toString();
 
 describe("parseDom", () => {
@@ -137,7 +141,11 @@ describe("parseDom", () => {
 
   test("should keep image within picture if it has a source", () => {
     const dom = new JSDOM(imgWithPictureHtml);
-    const parsed = parseDom(dom, "article377333", "article-img-within-picture.html");
+    const parsed = parseDom(
+      dom,
+      "article377333",
+      "article-img-within-picture.html"
+    );
     expect(parsed.sections.length).toBe(1);
     expect(parsed.sections[0].html).toContain(
       `<img src="https://travail-emploi.gouv.fr/local/adapt-img/1000/10x/IMG/jpg/shutterstock_702099169_2_.jpg" width="1000" height="563" alt="" class="adapt-img">`
@@ -151,16 +159,24 @@ describe("parseDom", () => {
     expect(parsed).toMatchSnapshot();
   });
 
+  test("should work with script intro", () => {
+    const dom = new JSDOM(articleWithScriptIntro);
+    const parsed = parseDom(dom, "article-abcd", "intro-qui-deconne-html");
+    expect(parsed).toMatchSnapshot();
+  });
+
   it.each`
-    input                                       | expected
-    ${"<button>hello</button>"}                 | ${""}
-    ${'<div class="oembed-source">hello</div>'} | ${""}
-    ${`<dt><!--[if IE]><img src='local/adapt-img/1024/10x/IMG/png/tableau-taux1.png?1667570454' width='1024' height='2532' alt='' class='adapt-img-ie ' /><![endif]--><!--[if !IE]><!--><picture class="adapt-img-wrapper c3054040965 png"><img src='data:image/svg+xml;base64,PiUlFHUTkvMW9KVWYvcm9BVmZZz4=' width='1024' height='2532' alt='' class='adapt-img adapt-img-multilayers blur' onmousedown='adaptImgFix(this)' /></picture><!--<![endif]--></dt>`} | ${"<dt><img src=\"https://travail-emploi.gouv.fr/local/adapt-img/1024/10x/IMG/png/tableau-taux1.png\" style=\"width:100%;height:auto;\"></dt>"}
-    ${`<dl class="spip_document_432065 spip_documents spip_documents_center"><dt><!--[if IE]><img src='local/adapt-img/1024/10x/IMG/jpg/creche_vip.jpg?1669818837' width='849' height='565' alt='' class='adapt-img-ie ' /><![endif]--><!--[if !IE]><!--><picture class="adapt-img-wrapper c831727054 jpg"><img src="data:image/svg+xml;base64,Pz0g==" width="849" height="565" alt="" class="adapt-img adapt-img-multilayers blur"></picture><!--<![endif]--></dt></dl>`} | ${"<dl class=\"spip_document_432065 spip_documents spip_documents_center\"><dt><img src=\"https://travail-emploi.gouv.fr/local/adapt-img/1024/10x/IMG/jpg/creche_vip.jpg\" style=\"width:100%;height:auto;\"></dt></dl>"}
-    ${`<dl class="spip_document_435034 spip_documents spip_documents_center"><dt><!--[if IE]><img src='local/adapt-img/1024/10x/IMG/jpg/shutterstock_702099169_2_.jpg?1669823553' width='1000' height='563' alt='' class='adapt-img-ie ' /><![endif]--><!--[if !IE]><!--><picture class="adapt-img-wrapper c208822612 jpg"><img src="data:image/svg+xml;base64,PHN2ZyB2aWV3Qm99zdmc+" width="1000" height="563" alt="" class="adapt-img adapt-img-multilayers blur"></picture><!--<![endif]--></dt></dl>`} | ${"<dl class=\"spip_document_435034 spip_documents spip_documents_center\"><dt><img src=\"https://travail-emploi.gouv.fr/local/adapt-img/1024/10x/IMG/jpg/shutterstock_702099169_2_.jpg\" style=\"width:100%;height:auto;\"></dt></dl>"}
-    ${`<picture class="adapt-img-wrapper c831727054 jpg" style="background-image:url(data:image/svg+xml;base64,PHN22Zz4=)">`} | ${`<picture class="adapt-img-wrapper c831727054 jpg" style="background-image:url(data:image/svg+xml;base64,PHN22Zz4=)"></picture>`}
-  `("$# should parse correctly", ({input, expected}) => {
-    const dom = new JSDOM(`<meta property="article:modified_time" content="2022-08-02" /><main><h1>Hello</h1><div class="main-article__texte texte--editorial crayon article-texte-377828 "><h3></h3>${input}</div></main>`);
+    input                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  | expected
+    ${"<button>hello</button>"}                                                                                                                                                                                                                                                                                                                                                                                                                                                                            | ${""}
+    ${'<div class="oembed-source">hello</div>'}                                                                                                                                                                                                                                                                                                                                                                                                                                                            | ${""}
+    ${`<dt><!--[if IE]><img src='local/adapt-img/1024/10x/IMG/png/tableau-taux1.png?1667570454' width='1024' height='2532' alt='' class='adapt-img-ie ' /><![endif]--><!--[if !IE]><!--><picture class="adapt-img-wrapper c3054040965 png"><img src='data:image/svg+xml;base64,PiUlFHUTkvMW9KVWYvcm9BVmZZz4=' width='1024' height='2532' alt='' class='adapt-img adapt-img-multilayers blur' onmousedown='adaptImgFix(this)' /></picture><!--<![endif]--></dt>`}                                           | ${'<dt><img src="https://travail-emploi.gouv.fr/local/adapt-img/1024/10x/IMG/png/tableau-taux1.png" style="width:100%;height:auto;"></dt>'}
+    ${`<dl class="spip_document_432065 spip_documents spip_documents_center"><dt><!--[if IE]><img src='local/adapt-img/1024/10x/IMG/jpg/creche_vip.jpg?1669818837' width='849' height='565' alt='' class='adapt-img-ie ' /><![endif]--><!--[if !IE]><!--><picture class="adapt-img-wrapper c831727054 jpg"><img src="data:image/svg+xml;base64,Pz0g==" width="849" height="565" alt="" class="adapt-img adapt-img-multilayers blur"></picture><!--<![endif]--></dt></dl>`}                                 | ${'<dl class="spip_document_432065 spip_documents spip_documents_center"><dt><img src="https://travail-emploi.gouv.fr/local/adapt-img/1024/10x/IMG/jpg/creche_vip.jpg" style="width:100%;height:auto;"></dt></dl>'}
+    ${`<dl class="spip_document_435034 spip_documents spip_documents_center"><dt><!--[if IE]><img src='local/adapt-img/1024/10x/IMG/jpg/shutterstock_702099169_2_.jpg?1669823553' width='1000' height='563' alt='' class='adapt-img-ie ' /><![endif]--><!--[if !IE]><!--><picture class="adapt-img-wrapper c208822612 jpg"><img src="data:image/svg+xml;base64,PHN2ZyB2aWV3Qm99zdmc+" width="1000" height="563" alt="" class="adapt-img adapt-img-multilayers blur"></picture><!--<![endif]--></dt></dl>`} | ${'<dl class="spip_document_435034 spip_documents spip_documents_center"><dt><img src="https://travail-emploi.gouv.fr/local/adapt-img/1024/10x/IMG/jpg/shutterstock_702099169_2_.jpg" style="width:100%;height:auto;"></dt></dl>'}
+    ${`<picture class="adapt-img-wrapper c831727054 jpg" style="background-image:url(data:image/svg+xml;base64,PHN22Zz4=)">`}                                                                                                                                                                                                                                                                                                                                                                              | ${`<picture class="adapt-img-wrapper c831727054 jpg" style="background-image:url(data:image/svg+xml;base64,PHN22Zz4=)"></picture>`}
+  `("$# should parse correctly", ({ input, expected }) => {
+    const dom = new JSDOM(
+      `<meta property="article:modified_time" content="2022-08-02" /><main><h1>Hello</h1><div class="main-article__texte texte--editorial crayon article-texte-377828 "><h3></h3>${input}</div></main>`
+    );
     const parsed = parseDom(dom, "id");
     expect(parsed.sections[0].html).toBe(expected);
   });
