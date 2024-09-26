18256ceb8562823391c14d70187de75e00047847
victor <victor.zeinstra@gmail.com>
2024-09-26 16:02:11 +0200

    chore: update tests

--- src/fetch-data/__tests__/parseDom.test.ts
+++ src/fetch-data/__tests__/parseDom.test.ts
@@ -6,11 +6,46 @@ import { parseDom } from "../parseDom";
 
 const sample = fs.readFileSync(path.join(__dirname, "article.html")).toString();
 
+const sampleNoSection = fs
+  .readFileSync(path.join(__dirname, "article-no-section.html"))
+  .toString();
+
+const sampleSectionsNoId = fs
+  .readFileSync(path.join(__dirname, "article-sections-no-id.html"))
+  .toString();
+
+const pictureHtml = fs
+  .readFileSync(path.join(__dirname, "article-picture.html"))
+  .toString();
+
+const imgWithPictureHtml = fs
+  .readFileSync(path.join(__dirname, "article-img-within-picture.html"))
+  .toString();
+
+const pictureHtmlSimple = fs
+  .readFileSync(path.join(__dirname, "article-picture-simple.html"))
+  .toString();
+
 describe("parseDom", () => {
   test("should parse HTML section", () => {
     const dom = new JSDOM(sample);
     const parsed = parseDom(dom, "article375531", "url-sample");
     expect(parsed).toMatchSnapshot();
+  });
+
+  test("should parse HTML with no section", () => {
+    const dom = new JSDOM(sampleNoSection);
+    const parsed = parseDom(dom, "article377849", "url-no-section");
+    expect(parsed).toMatchSnapshot();
+    expect(parsed.sections.length).toBe(1);
+    expect(parsed.sections[0].anchor).toBe("");
+  });
+
+  test("should parse HTML with intro section and 6 sections without id", () => {
+    const dom = new JSDOM(sampleSectionsNoId);
+    const parsed = parseDom(dom, "article112763", "url-with-sections-no-id");
+    expect(parsed).toMatchSnapshot();
+    expect(parsed.sections.length).toBe(7);
   });
 
   test("should throw if there is no main element", () => {
@@ -43,5 +78,42 @@ describe("parseDom", () => {
     expect(() => {
       parseDom(dom, "http://nomain");
     }).toThrowError();
+  });
+
+  test("should work with picture", () => {
+    const dom = new JSDOM(pictureHtmlSimple);
+    const parsed = parseDom(dom, "article377828", "simple-picture-html");
+    expect(parsed.sections.length).toBe(4);
+    expect(parsed.intro).toContain(
+      '<img src="https://travail-emploi.gouv.fr/sites/travail-emploi/files/styles/w_1200/public/2024-08/logo-activite-partielle.jpg.webp" width="800" height="450" alt="Activité partielle" loading="lazy" typeof="foaf:Image" class="fr-fluid-img">'
+    );
+    expect(parsed.sections[2].html).toContain(
+      '<img src="https://travail-emploi.gouv.fr/sites/travail-emploi/files/styles/w_1200/public/files-spip/png/ap_3.png.webp" width="450" height="187" alt="png/ap_3.png" loading="lazy" typeof="foaf:Image" class="fr-fluid-img">'
+    );
+    expect(parsed).toMatchSnapshot();
+  });
+
+  test("should work with picture more complex", () => {
+    const dom = new JSDOM(pictureHtml);
+    const parsed = parseDom(dom, "article375435", "picture-html");
+    expect(parsed.sections.length).toBe(3);
+    expect(parsed.intro).toContain(
+      "https://travail-emploi.gouv.fr/sites/travail-emploi/files/styles/w_1200/public/files-spip/jpg/creche_vip.jpg.webp"
+    );
+    expect(parsed).toMatchSnapshot();
+  });
+
+  test("should keep image within picture if it has a source", () => {
+    const dom = new JSDOM(imgWithPictureHtml);
+    const parsed = parseDom(
+      dom,
+      "article377333",
+      "article-img-within-picture.html"
+    );
+    expect(parsed.sections.length).toBe(1);
+    expect(parsed.intro).toContain(
+      `<img src="https://travail-emploi.gouv.fr/sites/travail-emploi/files/styles/w_1200/public/2024-05/Vignette-Guide-Op%C3%A9rations-de-modification-des-machines-.png.webp" width="800" height="500" alt="Guide technique relatif aux opérations de modification des machines ou des ensembles de machine en service" loading="lazy" typeof="foaf:Image" class="fr-fluid-img">`
+    );
+    expect(parsed).toMatchSnapshot();
   });
 });
